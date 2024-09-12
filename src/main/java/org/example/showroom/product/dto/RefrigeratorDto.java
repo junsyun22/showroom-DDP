1db83d9f76c1ab8c06df94aec9fddb26dd19e7de
junsyun22 <junsyun22@gmail.com>
2024-09-12 16:21:47 +0900

    Feat: 냉장고 dto 더미 Controller

--- src/main/java/org/example/showroom/product/dto/RefrigeratorDto.java
+++ src/main/java/org/example/showroom/product/dto/RefrigeratorDto.java
@@ -1,19 +1,21 @@
 package org.example.showroom.product.dto;
 
-// RefrigeratorDTO.java
-public class RefrigeratorDto {
-    private String productName;
-    private String productType;
-    private String installationType;
-    private String dimensions;
-    private double weight;
-    private int totalCapacity;
-    private int fridgeCapacity;
-    private int freezerCapacity;
-    private String energyEfficiencyRating;
-    private double powerConsumption;
+import lombok.Data;
 
-    // 생성자
+// RefrigeratorDTO.java
+@Data
+public class RefrigeratorDto {
+    private String productName; // 제품명
+    private String productType; // 제품 타입
+    private String installationType; // 설치 타입
+    private String dimensions; // 크기
+    private double weight; // 무게
+    private int totalCapacity; // 전체 용량
+    private int fridgeCapacity; // 냉장실 용량
+    private int freezerCapacity; // 냉동실 용량
+    private String energyEfficiencyRating;// 소비 효율 등급
+    private double powerConsumption; // 소비전력
+
     public RefrigeratorDto(String productName, String productType, String installationType,
                            String dimensions, double weight, int totalCapacity,
                            int fridgeCapacity, int freezerCapacity,
